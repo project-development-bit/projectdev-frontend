04a068111ecbeddadd13d6cf19a4505329c112b8
Aung Myo Paing <aungmyopaing890@gmail.com>
2025-11-07 14:55:23 +0700

    chat dialog CustomPointerInterceptor added

--- lib/features/chat/presentation/pages/right_chat_overlay.dart
+++ lib/features/chat/presentation/pages/right_chat_overlay.dart
@@ -1,6 +1,7 @@
 import 'package:cointiply_app/core/common/webview_wrapper.dart';
 import 'package:cointiply_app/core/localization/app_localizations.dart';
 import 'package:cointiply_app/features/chat/presentation/provider/right_chat_overlay_provider.dart';
+import 'package:cointiply_app/features/common/widgets/custom_pointer_interceptor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -21,25 +22,27 @@ class RightChatOverlay extends ConsumerWidget {
       return child;
     }
 
-    return Stack(
-      children: [
-        // Base app content
-        InkWell(
-            onTap: () {
-              ref.read(rightChatOverlayProvider.notifier).close();
-            },
-            child: child),
+    return CustomPointerInterceptor(
+      child: Stack(
+        children: [
+          // Base app content
+          InkWell(
+              onTap: () {
+                ref.read(rightChatOverlayProvider.notifier).close();
+              },
+              child: child),
 
-        // Right-side chat overlay
-        AnimatedPositioned(
-          duration: const Duration(milliseconds: 300),
-          top: 0,
-          bottom: 0,
-          right: isChatOpen ? 0 : -RightChatOverlayConfig.panelWidth,
-          width: RightChatOverlayConfig.panelWidth,
-          child: _ChatPanel(),
-        ),
-      ],
+          // Right-side chat overlay
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 300),
+            top: 0,
+            bottom: 0,
+            right: isChatOpen ? 0 : -RightChatOverlayConfig.panelWidth,
+            width: RightChatOverlayConfig.panelWidth,
+            child: _ChatPanel(),
+          ),
+        ],
+      ),
     );
   }
 }
