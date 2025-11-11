05a353a9a30996cbcfbc656828b01557c9b6e6cc
Aung Myo Paing <aungmyopaing890@gmail.com>
2025-11-11 15:24:42 +0700

    mobile drawer web profile route url error fix

--- lib/features/home/widgets/mobile_drawer.dart
+++ lib/features/home/widgets/mobile_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:cointiply_app/features/common/widgets/custom_pointer_interceptor.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -395,7 +396,7 @@ class MobileDrawer extends ConsumerWidget {
       ),
       onTap: () {
         Navigator.of(context).pop(); // Close drawer
-        context.pushNamedProfile();
+        kIsWeb ? context.goNamedProfile() : context.pushNamedProfile();
       },
     );
   }
