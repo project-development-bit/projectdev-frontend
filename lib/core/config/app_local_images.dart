d5c6631f9c6a8c15b243081ce810bb6d8c23b83c
Aungmyo Paing <62334147+aungmyopaing890@users.noreply.github.com>
2025-12-15 10:53:43 +0700

    onboardingbg mobile change

--- lib/core/config/app_local_images.dart
+++ lib/core/config/app_local_images.dart
@@ -23,25 +23,15 @@ class AppLocalImages {
 
   // Onboarding Images
   static const String onboardingBgMobile =
-      "assets/images/bg/onboarding_bg_mobile@2x.png";
+      "assets/images/bg/onboarding_bg_mobile@2x.jpg";
   static const String onboardingBgDesktop =
-      "assets/images/bg/onboarding_background@2x.jpg";
-
-  static const String onboardingCoinDesktop =
-      "assets/images/bg/on_boarding_coin@2x.png";
-
-  static const String onboardingCoinSection1Mobile =
-      "assets/images/bg/onboarding_coin_section1_mobile@2x.png";
-  static const String onboardingCoinSection2Mobile =
-      "assets/images/bg/onboarding_coin_section2_mobile@2x.png";
-  static const String onboardingCoinSection3Mobile =
-      "assets/images/bg/onboarding_coin_section3_mobile@2x.png";
+      "assets/images/bg/onboarding_bg_desktop@2x.jpg";
 
   // Home Images
   static const String homeBackgroundMobile =
       "assets/images/bg/home_background_mobile@2x.png";
   static const String homeBackgroundDesktop =
-      "assets/images/bg/home_background@2x.png";
+      "assets/images/bg/home_background_desktop@2x.png";
 
   static const String homeCoinBackgroundSection1Desktop =
       "assets/images/bg/coin_background_section_1_desktop@2x.png";
